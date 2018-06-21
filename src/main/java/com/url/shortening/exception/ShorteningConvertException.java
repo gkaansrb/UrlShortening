702e9217d4cdccff3e3a43d8c65b9973ee94fe07
kyu <gkaansrb@coupang.com>
2018-06-21 09:30:51 +0900

    testCode

--- src/main/java/com/url/shortening/exception/ShorteningConvertException.java
+++ src/main/java/com/url/shortening/exception/ShorteningConvertException.java
@@ -1,14 +1,7 @@
 package com.url.shortening.exception;
 
 public class ShorteningConvertException extends RuntimeException {
-	public ShorteningConvertException() {
-	}
-
 	public ShorteningConvertException(String message) {
 		super(message);
-	}
-
-	public ShorteningConvertException(String message, Throwable cause) {
-		super(message, cause);
 	}
 }
